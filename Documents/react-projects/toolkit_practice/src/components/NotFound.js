53287bc25adf71485e6de70df9f7fa9f9e9b06a7
trevorcschen <trevor@veecotech.com>
2021-06-08 01:01:33 +0800

    Removed FOlders

--- Documents/react-projects/toolkit_practice/src/components/NotFound.js
+++
@@ -1,5 +0,0 @@
-const NotFound = () => {
-    return (<div>Not FOund Page</div>);
-}
- 
-export default NotFound
--- Documents/react-projects/toolkit_practice/src/utils/calculation.js
+++
@@ -1,6 +0,0 @@
-export const handleFloat = (price) =>
-{
-    return parseFloat(price.toFixed(2)).toLocaleString(undefined,
-        {'minimumFractionDigits':2,'maximumFractionDigits':2})
-}
-
